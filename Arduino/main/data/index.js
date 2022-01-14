48ff8fc123d8b4a9b32df1c493eec4f9f6ecbbc9
CalebBap <caleb.bap@gmail.com>
2022-01-15 09:01:49 +1300

    Begin implementation of control options FSM. Enable and disable previous button when syncing with camera

--- Arduino/main/data/index.js
+++ Arduino/main/data/index.js
@@ -1,25 +1,42 @@
-var socket = new WebSocket('ws://10.100.0.200:81');
+try{
+    var socket = new WebSocket('ws://10.100.0.200:81');
+}
+catch(error){
+    console.error(error);
+}
+
 socket.onmessage = function(event){
     addConsoleText(event.data);
 }
 
-window.addEventListener('beforeunload', function (e) {
-	socket.close();
+window.addEventListener('load', function (e) {
+    fsm(0);
 });
 
-/*
+window.addEventListener('beforeunload', function (e) {
+	try{
+        socket.close();
+    }
+    catch(error){
+        console.error(error);
+    }
+});
+
 function syncCmd(){
-    socket.send("#sync");
-    document.getElementById("syncButton").disabled=true;
+    try{
+        socket.send("#sync");
+    }
+    catch(error){
+        console.error(error);
+    }
 }
-*/
 
 function prev(){
-    addConsoleText("Previous button pressed\n");
+    fsm_state -= 1;
 }
 
 function next(){
-    addConsoleText("Next button pressed\n");
+    fsm_state += 1;
 }
 
 function addConsoleText(text){
@@ -30,4 +47,30 @@ function addConsoleText(text){
 
 function clearTerminal(){
     document.getElementById("response_console").value = "";
+}
+
+function fsm(value){
+    if ( typeof fsm.fsm_state == 'undefined' ) {
+        fsm.fsm_state = 0;
+    }
+
+    fsm.fsm_state += value;
+
+    if(fsm.fsm_state < 0){
+        fsm.fsm_state = 0;
+    }
+
+    switch(fsm.fsm_state){
+        case 0:
+            document.getElementById("prev_control_bttn").style.pointerEvents = "none";
+            document.getElementById("prev_control_bttn").disabled = true;
+            document.getElementById("prev_control").style.opacity = "0.5";
+            break;
+        case 1:
+            syncCmd();
+            document.getElementById("prev_control_bttn").style.pointerEvents = "auto";
+            document.getElementById("prev_control_bttn").disabled = false;
+            document.getElementById("prev_control").style.opacity = "1";
+            break;
+    }
 }
